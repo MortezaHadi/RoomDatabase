59af45e47679bd027156b346f870e3cd327e196d
MortezaHadi <morteza_tanya@yahoo.com>
2022-12-26 19:14:03 +0330

    Fragments

---
+++ app/src/main/java/com/example/roomdatabase/fragment/add/AddFragment.kt
@@ -0,0 +1,27 @@
+package com.example.roomdatabase.fragment.add
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomdatabase.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class AddFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+
+
+
+
+
+        return view
+    }
+}
